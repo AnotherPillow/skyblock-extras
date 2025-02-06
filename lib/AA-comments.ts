d4d0566c82f11f90becd342cc14a9b1f9327114c
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2025-02-06 20:07:16 +0000

    Unhidden content nightblock nice colour

--- lib/AA-comments.ts
+++ lib/AA-comments.ts
@@ -4,7 +4,7 @@
 // @description A userscript to improve the skyblock.net forums experience!
 // @match       https://skyblock.net/*
 // @grant       none
-// @version     1.1.5
+// @version     1.1.6
 // @author      AnotherPillow
 // @license     GNU GPLv3
 // @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
